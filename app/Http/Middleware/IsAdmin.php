88a3a829bfef7c56d7ab444ba6f1ea0835e7136f
john_dev <johnhaimez314@gmail.com>
2022-07-04 09:42:48 +0200

    feat : add files IsAdmin for middleware

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class IsAdmin
+{
+    /**
+     * if user has admin role, it's OK else forbiden.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        // if user has role admin
+        if (auth()->user()->role->libelle == 'admin') {
+            return $next($request);
+        } else {
+            abort(403);
+        }
+    }
+}
